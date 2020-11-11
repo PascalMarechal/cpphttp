7160e817baaa3037caa3bef753d62eaf333da635
Pascal Marechal <contact@pascal-marechal.com>
2020-11-11 09:40:31 +0100

    matcher should be done for now.

--- src/internal/connection/match_end_of_header.h
+++ src/internal/connection/match_end_of_header.h
@@ -13,14 +13,35 @@ namespace cpphttp
                 Iterator begin, Iterator end) const
             {
                 Iterator i = begin;
-                while (i != end)
-                    if (c_ == *i++)
+                while (true)
+                    // \n\n => OK
+                    if (checkCurrent('\n', i, end) && checkNext('\n', i, end))
                         return std::make_pair(i, true);
+                    // \r\n => Check for \n or \r\n
+                    else if ((checkCurrent('\r', i, end) && checkNext('\n', i, end))
+                             // \n or \r\n
+                             && (checkNext('\n', i, end) || (checkCurrent('\r', i, end) && checkNext('\n', i, end))))
+                        return std::make_pair(i, true);
+                    else if (i == end)
+                        break;
+                    else if (!checkCurrent('\r', i, end))
+                        ++i;
+
                 return std::make_pair(i, false);
             }
 
         private:
-            char c_ = 'm';
+            template <typename Iterator>
+            inline bool checkNext(char c, Iterator &current, Iterator end) const
+            {
+                return current++ != end && *current == c;
+            }
+
+            template <typename Iterator>
+            inline bool checkCurrent(char c, Iterator current, Iterator end) const
+            {
+                return current != end && *current == c;
+            }
         };
     } // namespace internal
 } // namespace cpphttp
--- tests/internal/connection/match_end_of_header_test.cpp
+++ tests/internal/connection/match_end_of_header_test.cpp
@@ -3,10 +3,68 @@
 
 using namespace cpphttp::internal;
 
-TEST(MATCHER, FailToFindEnd)
+TEST(Matcher, FailToFindEnd)
 {
-    std::string no_end_in_string = "eazeazeaz\n";
+    std::string no_end_in_string = "something\n";
     match_end_of_header matcher;
     auto result = matcher(no_end_in_string.cbegin(), no_end_in_string.cend());
     EXPECT_FALSE(result.second);
+    no_end_in_string = "something\r\n";
+    result = matcher(no_end_in_string.cbegin(), no_end_in_string.cend());
+    EXPECT_FALSE(result.second);
+    no_end_in_string = "something\r\n\r";
+    result = matcher(no_end_in_string.cbegin(), no_end_in_string.cend());
+    EXPECT_FALSE(result.second);
+    no_end_in_string = "something\r\n\r\r";
+    result = matcher(no_end_in_string.cbegin(), no_end_in_string.cend());
+    EXPECT_FALSE(result.second);
+    no_end_in_string = "something\r\n\r\r\n";
+    result = matcher(no_end_in_string.cbegin(), no_end_in_string.cend());
+    EXPECT_FALSE(result.second);
+    no_end_in_string = "something\r\n\r\r\n\r";
+    result = matcher(no_end_in_string.cbegin(), no_end_in_string.cend());
+    EXPECT_FALSE(result.second);
+}
+
+TEST(Matcher, FindEndOfHeaderWithCRLF)
+{
+    std::string CRLF_end_in_string = "something\r\n\r\n";
+    match_end_of_header matcher;
+    auto result = matcher(CRLF_end_in_string.cbegin(), CRLF_end_in_string.cend());
+    EXPECT_TRUE(result.second);
+    EXPECT_TRUE(CRLF_end_in_string.cend() - 1 == result.first);
+
+    CRLF_end_in_string = "something\r\n\r\r\n\r\n";
+    result = matcher(CRLF_end_in_string.cbegin(), CRLF_end_in_string.cend());
+    EXPECT_TRUE(result.second);
+    EXPECT_TRUE(CRLF_end_in_string.cend() - 1 == result.first);
+
+    CRLF_end_in_string = "\r\n\r\nsomething";
+    result = matcher(CRLF_end_in_string.cbegin(), CRLF_end_in_string.cend());
+    EXPECT_TRUE(result.second);
+    EXPECT_TRUE(CRLF_end_in_string.cbegin() + 3 == result.first);
+}
+
+TEST(Matcher, FindEndOfHeaderWithLFOnly)
+{
+    std::string LF_end_in_string = "something\r\n\n";
+    match_end_of_header matcher;
+    auto result = matcher(LF_end_in_string.cbegin(), LF_end_in_string.cend());
+    EXPECT_TRUE(result.second);
+    EXPECT_TRUE(LF_end_in_string.cend() - 1 == result.first);
+
+    LF_end_in_string = "something\n\n";
+    result = matcher(LF_end_in_string.cbegin(), LF_end_in_string.cend());
+    EXPECT_TRUE(result.second);
+    EXPECT_TRUE(LF_end_in_string.cend() - 1 == result.first);
+
+    LF_end_in_string = "something\nsomething\nsomething\n\n";
+    result = matcher(LF_end_in_string.cbegin(), LF_end_in_string.cend());
+    EXPECT_TRUE(result.second);
+    EXPECT_TRUE(LF_end_in_string.cend() - 1 == result.first);
+
+    LF_end_in_string = "\n\nsomething";
+    result = matcher(LF_end_in_string.cbegin(), LF_end_in_string.cend());
+    EXPECT_TRUE(result.second);
+    EXPECT_TRUE(LF_end_in_string.cbegin() + 1 == result.first);
 }
