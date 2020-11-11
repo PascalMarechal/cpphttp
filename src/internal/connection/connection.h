400301da644556f579f43437378db67da28c0267
Pascal Marechal <contact@pascal-marechal.com>
2020-11-11 15:32:22 +0100

    small refactoring + filling buffer in test.

--- src/internal/connection/connection.h
+++ src/internal/connection/connection.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <functional>
 #include "internal/asio.h"
-//#include "request/request.h"
+#include "request/request.h"
 #include "match_end_of_header.h"
 
 namespace cpphttp
@@ -18,21 +18,27 @@ namespace cpphttp
 
             void start()
             {
-                m_async_functions.async_read_until(m_socket, asio::dynamic_buffer(m_headerBuffer), m_matcher, std::bind(&connection::onRead, this->shared_from_this(), std::placeholders::_1, std::placeholders::_2));
+                readHeader();
                 //asio::async_read_until(m_socket, asio::dynamic_buffer(m_headerBuffer), matcher, std::bind(&connection::onRead, this, std::placeholders::_1, std::placeholders::_2));
             }
 
         private:
-            void onRead(asio::error_code error, std::size_t bytes_transferred)
+            inline void readHeader()
             {
-                std::cout << "READ\n";
+                m_asyncFunctions.async_read_until(m_socket, asio::dynamic_buffer(m_headerBuffer), m_matcher, std::bind(&connection::onReadHeader, this->shared_from_this(), std::placeholders::_1, std::placeholders::_2));
+            }
+
+            void onReadHeader(asio::error_code error, std::size_t bytes_transferred)
+            {
+                
             }
 
             asio::ip::tcp::socket m_socket;
             std::string m_headerBuffer;
 
-            AsyncFunctions m_async_functions;
+            AsyncFunctions m_asyncFunctions;
             static inline match_end_of_header m_matcher;
+            std::unique_ptr<cpphttp::request::request> m_currentReq;
         };
     } // namespace internal
 } // namespace cpphttp
--- tests/internal/connection/connection_test.cpp
+++ tests/internal/connection/connection_test.cpp
@@ -4,6 +4,8 @@
 
 using namespace cpphttp::internal;
 
+extern std::string postRequestHeader;
+
 class AsyncFunctions
 {
 public:
@@ -19,7 +21,10 @@ public:
   void CreateFakeMethods()
   {
     ON_CALL(*this, async_read_until).WillByDefault([this](asio::ip::tcp::socket &socket, asio::dynamic_string_buffer<char, std::char_traits<char>, std::allocator<char>> buffer, match_end_of_header &matcher, std::function<void(std::error_code, std::size_t)> callback) {
-      std::cout << "Hello There Read\r\n\r\n";
+      // Copy header
+      buffer.grow(postRequestHeader.size());
+      memcpy(buffer.data(0, postRequestHeader.size()).data(), postRequestHeader.c_str(), postRequestHeader.size());
+      // Call handler
       callback(std::error_code(), buffer.size());
     });
 
