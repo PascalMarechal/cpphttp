97626ae2492a982053f4b01fbf7d64d1ed69421e
Pascal Marechal <contact@pascal-marechal.com>
2020-11-12 14:40:01 +0100

    more work on connection.

--- src/internal/connection/connection.h
+++ src/internal/connection/connection.h
@@ -8,13 +8,12 @@ namespace cpphttp
 {
     namespace internal
     {
-        template <typename ConnectionFunctions>
-        class connection : public std::enable_shared_from_this<connection<ConnectionFunctions>>
+        template <typename ConnectionFunctions, typename Router>
+        class connection : public std::enable_shared_from_this<connection<ConnectionFunctions, Router>>
         {
         public:
-            using buffer_iterator = asio::buffers_iterator<asio::streambuf::const_buffers_type>;
-
-            connection(asio::ip::tcp::socket &&sock, const ConnectionFunctions &functions) : m_socket(std::move(sock)), m_functions(functions){};
+            connection(asio::ip::tcp::socket &&sock, const ConnectionFunctions &functions, const Router &router)
+                : m_socket(std::move(sock)), m_functions(functions), m_router(router){};
 
             void start()
             {
@@ -28,16 +27,42 @@ namespace cpphttp
                 m_functions.async_read_until(m_socket, asio::dynamic_buffer(m_headerBuffer), m_matcher, std::bind(&connection::onReadHeader, this->shared_from_this(), std::placeholders::_1, std::placeholders::_2));
             }
 
+            inline void readBody()
+            {
+                m_currentReq.setHeader(m_headerBuffer);
+                m_headerBuffer.clear();
+                m_functions.async_read_exactly(m_socket, asio::dynamic_buffer(m_bodyBuffer), asio::transfer_exactly(m_currentReq.getHeader()->getExpectedBodySize()), std::bind(&connection::onReadBody, this->shared_from_this(), std::placeholders::_1, std::placeholders::_2));
+            }
+
             void onReadHeader(asio::error_code error, std::size_t bytes_transferred)
             {
+                readBody();
+            }
+
+            void onReadBody(asio::error_code error, std::size_t bytes_transferred)
+            {
+                // asio::post(m_socket.get_executor(), [reqCapute = std::move(this->m_currentReq), this]() mutable -> void {
+                //     std::cout << "LOL";
+                //     m_router.process(std::move(reqCapute));
+                // });
+                m_functions.async_work(m_socket.get_executor(), [this]() mutable -> void {
+                    this->processRequests();
+                });
+            }
+
+            void processRequests()
+            {
+                m_router.process(std::move(m_currentReq));
             }
 
             asio::ip::tcp::socket m_socket;
             std::string m_headerBuffer;
+            std::string m_bodyBuffer;
             const ConnectionFunctions &m_functions;
+            const Router &m_router;
 
             static inline match_end_of_header m_matcher;
-            std::unique_ptr<cpphttp::request::request> m_currentReq;
+            cpphttp::request::request m_currentReq;
         };
     } // namespace internal
 } // namespace cpphttp
--- tests/internal/connection/connection_test.cpp
+++ tests/internal/connection/connection_test.cpp
@@ -2,22 +2,53 @@
 #include <gmock/gmock.h>
 #include "internal/connection/connection.h"
 #include "common/requests.h"
+#include "request/request.h"
 
 using namespace cpphttp::internal;
+
+class RouterMock
+{
+public:
+  MOCK_CONST_METHOD1(process, std::string(cpphttp::request::request &&));
+};
 
 class ConnectionFunctionsMock
 {
 public:
   MOCK_CONST_METHOD4(async_read_until, void(asio::ip::tcp::socket &, asio::dynamic_string_buffer<char, std::char_traits<char>, std::allocator<char>>, match_end_of_header &, std::function<void(std::error_code, std::size_t)>));
+  MOCK_CONST_METHOD4(async_read_exactly, void(asio::ip::tcp::socket &, asio::dynamic_string_buffer<char, std::char_traits<char>, std::allocator<char>>, asio::detail::transfer_exactly_t, std::function<void(std::error_code, std::size_t)>));
+  MOCK_CONST_METHOD2(async_work, void(asio::any_io_executor, std::function<void(void)>));
 
-  void createFakeReadMethod(uint32_t expectedReadCalls)
+  void createFakeReadMethods()
+  {
+    createFakeReadUntilMethod();
+    createFakeReadExactlyMethod();
+  }
+
+  void createFakeReadUntilMethod()
   {
     ON_CALL(*this, async_read_until).WillByDefault([this](asio::ip::tcp::socket &socket, asio::dynamic_string_buffer<char, std::char_traits<char>, std::allocator<char>> buffer, match_end_of_header &matcher, std::function<void(std::error_code, std::size_t)> callback) {
-      // Copy header
       buffer.grow(postRequestHeader.size());
       memcpy(buffer.data(0, postRequestHeader.size()).data(), postRequestHeader.c_str(), postRequestHeader.size());
       // Call handler
       callback(std::error_code(), buffer.size());
+    });
+  }
+
+  void createFakeReadExactlyMethod()
+  {
+    ON_CALL(*this, async_read_exactly).WillByDefault([this](asio::ip::tcp::socket &socket, asio::dynamic_string_buffer<char, std::char_traits<char>, std::allocator<char>> buffer, asio::detail::transfer_exactly_t end, std::function<void(std::error_code, std::size_t)> callback) {
+      buffer.grow(postRequestBody.size());
+      memcpy(buffer.data(0, postRequestBody.size()).data(), postRequestBody.c_str(), postRequestBody.size());
+      // Call handler
+      callback(std::error_code(), buffer.size());
+    });
+  }
+
+  void createFakeAsyncMethod()
+  {
+    ON_CALL(*this, async_work).WillByDefault([this](asio::any_io_executor e, std::function<void(void)> callback) {
+      callback();
     });
   }
 };
@@ -26,17 +57,24 @@ TEST(Connection, Creation)
 {
   asio::io_context context;
   asio::ip::tcp::socket sock(context);
-  ConnectionFunctionsMock mock;
-  EXPECT_CALL(mock, async_read_until).Times(0);
-  auto c = std::make_shared<connection<ConnectionFunctionsMock>>(std::move(sock), mock);
+  ConnectionFunctionsMock functionsMock;
+  RouterMock routerMock;
+  EXPECT_CALL(functionsMock, async_read_until).Times(0);
+  std::make_shared<connection<ConnectionFunctionsMock, RouterMock>>(std::move(sock), functionsMock, routerMock);
 }
 
-TEST(Connection, Read)
+TEST(Connection, ReadRequestWithBody)
 {
   asio::io_context context;
   asio::ip::tcp::socket sock(context);
-  ConnectionFunctionsMock mock;
-  EXPECT_CALL(mock, async_read_until).Times(1);
-  auto c = std::make_shared<connection<ConnectionFunctionsMock>>(std::move(sock), mock);
+  ConnectionFunctionsMock functionsMock;
+  functionsMock.createFakeReadMethods();
+  functionsMock.createFakeAsyncMethod();
+  RouterMock routerMock;
+  EXPECT_CALL(functionsMock, async_read_until).Times(1);
+  EXPECT_CALL(functionsMock, async_read_exactly).Times(1);
+  EXPECT_CALL(functionsMock, async_work).Times(1);
+  EXPECT_CALL(routerMock, process).Times(1);
+  auto c = std::make_shared<connection<ConnectionFunctionsMock, RouterMock>>(std::move(sock), functionsMock, routerMock);
   c->start();
 }
