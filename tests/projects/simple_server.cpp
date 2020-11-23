78f2bf45ff72dd4786976facfbeb6265bf365504
Pascal Marechal <contact@pascal-marechal.com>
2020-11-23 14:30:01 +0100

    simple test project added.

---
+++ tests/projects/simple_server.cpp
@@ -0,0 +1,46 @@
+#include "cpphttp.h"
+
+using namespace cpphttp::server;
+using namespace cpphttp::request;
+using namespace cpphttp::response;
+
+int main(void)
+{
+    // Create a server at port 8080
+    server myserver(8080);
+
+    // Create a simple router with several routes
+    // One main page
+    // Form get & post path
+    router myrouter;
+    myrouter.onGet("/", [](cpphttp::request::request &, cpphttp::response::response &res, error_callback) {
+        res.send("<h1>Welcome to simple server</h1><a href='/form'>Go to form page</a>");
+    });
+
+    myrouter.onGet("/form", [](cpphttp::request::request &, cpphttp::response::response &res, error_callback) {
+        res.send("<h1>Test form</h1>"
+                 "<form action='/form' method='post'><div><label for='name'>Name :</label>"
+                 "<input type='text' id='name' name='user_name'>"
+                 "</div>"
+                 "<div>"
+                 "<label for='mail'>Email :</label>"
+                 "<input type='email' id='mail' name='user_mail'>"
+                 "</div>"
+                 "<div class='button'>"
+                 "<button type='submit'>Send message</button>"
+                 "</div>"
+                 "</form>"
+                 "<a href='/'>Go back to home page</a>");
+    });
+
+    myrouter.onPost("/form", [](cpphttp::request::request &req, cpphttp::response::response &res, error_callback) {
+        res.send("<h1>Form has been sent to the server</h1><a href='/'>Go back to home page</a>");
+    });
+
+    // Set the router to the server
+    myserver.setRouter(std::move(myrouter));
+
+    // Start the server
+    myserver.start();
+    return 0;
+}
